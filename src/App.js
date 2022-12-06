0fe627e471bc6328a2f902738dc20d8d07818e06
Deniansyah <deniansyah814@gmail.com>
2022-12-07 01:12:05 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./components/counter"
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = React.useState(1)
+    const [showAlert, setShowAlert] = React.useState(false)
+    const condition = (val, cb) => {
+        if (val >= 1 && val <= 10) {
+            setShowAlert(false)
+            cb();
+        } else {
+            setShowAlert(true)
+        }
+    };
+
+    const decrement = () => {
+        const newVal = counter - 1
+        condition(newVal, () => {
+            setCounter(newVal)
+        })
+    }
+
+    const increment = () => {
+        const newVal = counter + 1
+        condition(newVal, () => {
+            setCounter(newVal)
+        })
+    }
+
+    return(
+        <div className="wrapper">
+            {showAlert && <div className="alert">min / max exceeded</div>}
+            <div className="child-wrapper">
+                <div onClick={decrement} className="btn">
+                    <button>-</button>
+                </div>
+                <Counter>{counter}</Counter>
+                <div onClick={increment} className="btn">
+                    <button>+</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Counter = (props) => {
+    return(
+        <div>{props.children}</div>
+    )
+}
+
+export default Counter
